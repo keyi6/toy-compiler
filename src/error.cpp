17e93041b942494ff73ec2b14a4e5a31863b4410
cjhahaha <cj_lee_canu@hotmail.com>
2018-11-10 16:36:12 +0800

    add Error class

---
+++ src/error.cpp
@@ -0,0 +1,21 @@
+//
+// Created by cj on 2018/11/10.
+//
+
+
+#include "error.hpp"
+
+#include <iostream>
+using namespace std;
+
+
+Error::Error(int _lineNumber, string _errorMsg) {
+    lineNumber = _lineNumber;
+    errorMsg = _errorMsg;
+}
+
+
+void Error::output() {
+    cout << "[Line " << lineNumber << "]" << endl;
+    cout << "    " << errorMsg << endl;
+}
---
+++ src/error.hpp
@@ -0,0 +1,23 @@
+//
+// Created by cj on 2018/11/10.
+//
+
+#ifndef TOY_C_COMPILER_ERROR_HPP
+#define TOY_C_COMPILER_ERROR_HPP
+
+#include <string>
+using namespace std;
+
+
+class Error {
+private:
+    int lineNumber;
+    string errorMsg;
+
+public:
+    Error(int _lineNumber, string _errorMsg);
+    void output();
+};
+
+
+#endif //TOY_C_COMPILER_ERROR_HPP
